c3e597dd6872bcba1e5d0a897e53fe5b1b98aec6
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-06-06 20:25:57 +0000

    Users can sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    def new
+     @user = User.new
+    end
+    
+    def create
+
+     @user = User.new
+     @user.username = params[:user][:username]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+ 
+
+     if @user.save
+       flash[:notice] = "Welcome to Bloccit #{@user.username}!"
+       redirect_to root_path
+     else
+       flash.now[:alert] = "There was an error creating your account. Please try again."
+       render :new
+     end
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
   
+  resources :users
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
