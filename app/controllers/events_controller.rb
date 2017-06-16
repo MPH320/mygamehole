a1d967a738906205e52674c4e57754086a7470e9
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-06-16 15:28:17 +0000

    Value added to Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,19 @@
 class EventsController < ApplicationController
     
     def create
+        puts "Starting evaluation"
+        if params[:value].nil?
+            params[:value] = nil
+            puts "no value"
+        else
+            params[:value].to_i
+            puts "there is a value"
+        end
       
-       @event = Event.create!(
+        @event = Event.create!(
             name:     params[:name],
-            game_id:  params[:game_id],
+            value:    params[:value],
+            game_id:  get_game().id,
             user_id:  current_user.id
         )
         
@@ -13,7 +22,7 @@ class EventsController < ApplicationController
                 puts "Analytics saved"
                 format.js
             end
-end
+        end
      
      
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615205417) do
+ActiveRecord::Schema.define(version: 20170616143732) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170615205417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "value"
   end
 
   add_index "events", ["game_id"], name: "index_events_on_game_id"
